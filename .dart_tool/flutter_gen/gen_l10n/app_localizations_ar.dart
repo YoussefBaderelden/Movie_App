f179483d15b60fb3a53f862645fee3156ef41090
nada ayman <nada.ayman.elsayed95@gmail.com>
2025-02-20 21:50:14 +0200

    logIn,register,forgetPassword

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,54 @@
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get email => 'الحساب';
+
+  @override
+  String get password => 'كلمة المرور';
+
+  @override
+  String get forgetYourPassword => 'نسيت كلمة السر';
+
+  @override
+  String get logIn => 'تسجيل الدخول';
+
+  @override
+  String get dontHaveAccount => 'ليس لديك حساب';
+
+  @override
+  String get createAccount => 'انشاء حساء جديد';
+
+  @override
+  String get or => 'أو';
+
+  @override
+  String get logInWithGoogle => 'تسجيل الدخول بحساب جوجل';
+
+  @override
+  String get enterValidEmail => 'ادخل حساب صالح';
+
+  @override
+  String get name => 'الاسم';
+
+  @override
+  String get confirmPassword => 'تأكيد كلمة المرور';
+
+  @override
+  String get phoneNumber => 'رقم التليفون';
+
+  @override
+  String get alreadyHaveAccount => 'لديك حساب بالفعل';
+
+  @override
+  String get verifyMail => 'الحساب';
+
+  @override
+  String get register => 'التسجيل';
+
+  @override
+  String get enterYourPassword => 'ادخل كلمة المرور';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,54 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get email => 'E-Mail';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get forgetYourPassword => 'Forget your Password';
+
+  @override
+  String get logIn => 'Log in';
+
+  @override
+  String get dontHaveAccount => 'Don\'t have Account';
+
+  @override
+  String get createAccount => 'Create Account';
+
+  @override
+  String get or => 'Or';
+
+  @override
+  String get logInWithGoogle => 'Log in With Google';
+
+  @override
+  String get enterValidEmail => 'Enter valid E-Mail';
+
+  @override
+  String get name => 'Name';
+
+  @override
+  String get confirmPassword => 'Confirm Password';
+
+  @override
+  String get phoneNumber => 'Phone Number';
+
+  @override
+  String get alreadyHaveAccount => 'Already Have Account';
+
+  @override
+  String get verifyMail => 'Verify E-mail';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get enterYourPassword => 'Enter Your Password';
+}
